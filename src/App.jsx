5da6109932c755dca5e7371db6ee84cd1d419f6a
qAbdullajon <azikmelor7705@gmail.com>
2025-03-24 16:34:55 +0500

    finish darslik qo'shish

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/main";
 import Login from "./pages/login";
-import Darslar from "./admin/pages/darslar";
 import AdminLogin from "./admin/pages/login";
 import AdminHome from "./admin/pages/home";
 import AdminLayout from "./admin/pages/layout";
@@ -10,6 +9,9 @@ import Testlar from "./admin/pages/testlar";
 import Analitika from "./admin/pages/analitika";
 import Topshiriqlar from "./admin/pages/topshiriqlar";
 import Maqolalarim from "./admin/pages/maqolalarim";
+import Darsliklar from "./admin/pages/darsliklar";
+import AddDarslik from "./admin/pages/add-darslik";
+import AddTest from "./admin/pages/add-test";
 
 export default function App() {
   return (
@@ -20,8 +22,10 @@ export default function App() {
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/admin/home" element={<AdminHome />} />
         <Route path="/admin" element={<AdminLayout />}>
-          <Route path="/admin/darsliklar" element={<Darslar />} />
+          <Route path="/admin/darsliklar" element={<Darsliklar />} />
+          <Route path="/admin/add-darslik" element={<AddDarslik />} />
           <Route path="/admin/testlar" element={<Testlar />} />
+          <Route path="/admin/add-test" element={<AddTest />} />
           <Route path="/admin/analitika" element={<Analitika />} />
           <Route path="/admin/topshiriqlar" element={<Topshiriqlar />} />
           <Route path="/admin/maqolalarim" element={<Maqolalarim />} />
