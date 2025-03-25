1aa473ba4e57daea4174019037ed0c994616884b
CyberHacker2011 <ibrohimshaymardanov011@outlook.com>
2025-03-25 17:37:30 +0500

    adding buttons to articlepage

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -24,17 +24,20 @@ const ArticlePage = () => {
     
     <>
         <PageHeader title="Teskari Proporsional Miqdorlar" other={returnBtn} />
-        <main className="flex flex-wrap xl:flex-nowrap justify-around py-5">
-          <div className="flex flex-col max-w-xl xl:max-w-[632px] gap-5">
-            <h1 className="text-xl lg:text-2xl font-bold ">{article.title}</h1>
+        <main className="flex flex-wrap justify-evenly p-10 gap-y-5">
+          <div className="flex flex-col max-w-[632px] gap-5">
+            <h1 className="text-2xl font-bold ">{article.title}</h1>
             <img src={image} alt="Article image" />
-            <p className="text-base lg:text-lg font-medium">{article.text}</p>
+            <p className="text-lg font-medium">{article.text}</p>
+            <div className="flex w-full justify-between">
+              <button className='bg-[#E2EAFF] text-[#3857AF] px-12 py-3 rounded-[10px] [box-shadow: 0px 0px 11.95px 0px #FFFFFF5] hover:opacity-75 cursor-pointer w-[49%]' onClick={()=>navigate(`/dashboard/articles/${1}`)}>Oldingi</button>
+              <button className='bg-[#3857AF] text-white px-12 py-3 rounded-[10px] [box-shadow: 0px 0px 11.95px 0px #FFFFFF5] hover:opacity-75 cursor-pointer w-[49%]' onClick={()=>navigate(`/dashboard/articles/${1}`)}>Keyingi</button>
+            </div>
           </div>
           <section className="flex flex-col border_stroke rounded-lg py-3 bg-white">
-            <div className="flex border-b-1 border-[#E7E7E7] justify-center items-center p-5"><h1 className="text-xl lg:text-2xl font-medium">Tavsiya etiladigan maqolalar</h1></div>
+            <div className="flex border-b-1 border-[#E7E7E7] justify-center items-center p-5"><h1 className="text-2xl font-medium">Tavsiya etiladigan maqolalar</h1></div>
             
             <div className="flex flex-col gap-2 p-3">
-              <RecommendedCard />
               <RecommendedCard />
               <RecommendedCard />
               <RecommendedCard />
